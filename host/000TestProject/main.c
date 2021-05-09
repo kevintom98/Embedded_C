ddce4605d7305cd7b99e1e9f262118f5ce9b6b74
Kevin Tom <kevintom98@gmail.com>
2021-05-09 23:17:02 +0530

    First push

---
+++ host/000TestProject/main.c
@@ -0,0 +1,20 @@
+/*
+ * main.c
+ *
+ *  Created on: 09-May-2021
+ *      Author: Kevin Tom
+ */
+
+
+#include<stdio.h>
+
+int main(void)
+{
+	printf("Hello World\n");
+
+	printf("Press any key to exit this application\n");
+
+	getchar();
+
+	return 0;
+}
