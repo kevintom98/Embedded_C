dc4b37daf5d73e9cca1c7fa39cfc25507c1b39a0
Kevin Tom <kevintom98@gmail.com>
2021-05-14 12:05:46 +0530

    Math program

---
+++ host/math_functions/math.c
@@ -0,0 +1,33 @@
+/*
+ * math.c
+ *
+ *  Created on: 14-May-2021
+ *      Author: Kevin Tom
+ */
+
+
+
+
+//Function to add two numbers
+int math_add(int n1, int n2)
+{
+	return n1+n2;
+}
+
+//Function to subtract two numbers
+int math_sub(int n1, int n2)
+{
+	return n1-n2;
+}
+
+//Function to multiply two numbers
+long long int math_mul(int n1, int n2)
+{
+	return (long long int)n1*n2;
+}
+
+//Function to divide two numbers
+float math_div(int n1, int n2)
+{
+	return n1/n2;
+}
---
+++ host/math_functions/math.h
@@ -0,0 +1,20 @@
+/*
+ * math.h
+ *
+ *  Created on: 14-May-2021
+ *      Author: Kevin Tom
+ */
+
+#ifndef MATH_H_
+#define MATH_H_
+
+
+
+int math_add(int n1, int n2);
+int math_sub(int n1, int n2);
+long long int math_mul(int n1, int n2);
+float math_div(int n1, int n2);
+
+
+
+#endif /* MATH_H_ */
